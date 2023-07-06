c3ec53300636c9e7d2df803df4e821a7f922f875
Liubov1102 <laav.odessa@gmail.com>
2023-07-06 09:35:30 +0200

    first

---
+++ js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+  body: document.querySelector("body"),
+  footer: document.querySelector(".footer"),
+  langSelector: document.querySelector("#lang"),
+  html: document.querySelector("html"),
+};
